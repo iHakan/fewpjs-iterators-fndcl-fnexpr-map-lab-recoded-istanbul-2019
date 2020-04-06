1e08b8b1aae8a28c234f5e99f8a25601c6a703f0
Hakan Yildiz <hakan@re-coded.com>
2020-04-06 08:09:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCase(str) {
+   var splitStr = str.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       // You do not need to check if i is larger than splitStr length, as your for does that for you
+       // Assign it back to the array
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   // Directly return the joined string
+   return splitStr.join(' '); 
 }
+
+  let newArr = tutorials.map(titleCase);
+  return newArr;
+  
+  
