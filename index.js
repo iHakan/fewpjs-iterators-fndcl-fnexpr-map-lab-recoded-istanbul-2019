a360382758414e9637c1f14e5b5597ee9dd44c7a
Hakan Yildiz <hakan@re-coded.com>
2020-04-06 08:27:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 function titleCased(text) {
-    let wordsArray = text.split(' ')
+    let wordsArray = text.split(' ');
     let capsArray = wordsArray.map(word=>{
         return word[0].toUpperCase() + word.slice(1)
-    })
-    return capsArray.join(' ')
+    });
+    return capsArray.join(' ');
 }
   
