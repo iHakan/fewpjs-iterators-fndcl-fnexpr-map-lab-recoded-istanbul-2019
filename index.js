dfa96d1c920e2d5a1d6a866fc4408ea2ef147968
Hakan Yildiz <hakan@re-coded.com>
2020-04-06 08:33:17 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,9 +0,0 @@
-
-function titleCased(text) {
-    let wordsArray = text.split(' ');
-    let capsArray = wordsArray.map(word=>{
-        return word[0].toUpperCase() + word.slice(1)
-    });
-    return capsArray.join(' ');
-}
-  
